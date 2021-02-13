282def462b23d91f37ba3761aebd0538d90b92b0
Filip Hodun <filiphodun@gmail.com>
2021-02-13 22:31:43 +0100

    some code, don't work fully

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/fhodun/GObank
 
 go 1.15
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gorilla/sessions v1.2.1
+	github.com/sirupsen/logrus v1.7.0
+)
--- main.go
+++ main.go
@@ -1,93 +1,144 @@
-// Copyright 2010 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// +build ignore
 /*
+	PROGRAM DOESN'T FULLY WORK FOR NOW
+	PROGRAM DOESN'T FULLY WORK FOR NOW
+	PROGRAM DOESN'T FULLY WORK FOR NOW
+	PROGRAM DOESN'T FULLY WORK FOR NOW
+	PROGRAM DOESN'T FULLY WORK FOR NOW
+*/
+
 package main
 
 import (
+	"database/sql"
 	"html/template"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"regexp"
+	"os"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/sessions"
+	log "github.com/sirupsen/logrus"
 )
 
-type Page struct {
-	Title string
-	Body  []byte
+// LogIn dupa
+type LogIn struct {
+	Username string
+	Password string
 }
 
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
+// User dupa
+type User struct {
+	id        int
+	username  string
+	password  string
+	createdAt time.Time
 }
 
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
+var createTableQuery = `
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    created_at DATETIME,
+    PRIMARY KEY (id)
+);
+`
+
+func dbInitConn() *sql.DB {
+	db, err := sql.Open("mysql", "root:@(127.0.0.1:3306)/gobank?parseTime=true")
 	if err != nil {
-		return nil, err
+		log.Error("Unsuccessful connection open with database, ", err)
 	}
-	return &Page{Title: title, Body: body}, nil
+	return db
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+var db = dbInitConn()
+
+var (
+	// key must be 16, 24 or 32 bytes long (AES-128, AES-192 or AES-256)
+	key   = []byte("super-secret-key")
+	store = sessions.NewCookieStore(key)
+)
+
+// Account dupa
+func Account(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "dupa")
+
+	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+		http.Error(w, "Forbidden", http.StatusForbidden)
 		return
 	}
-	renderTemplate(w, "view", p)
+
+	println(w, "The cake is a lie!")
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		p = &Page{Title: title}
-	}
-	renderTemplate(w, "edit", p)
-}
-
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-	body := r.FormValue("body")
-	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+// Login dupa
+func Login(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("./site/account.html"))
+	if r.Method != http.MethodPost {
+		tmpl.Execute(w, nil)
 		return
 	}
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)
-}
 
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+	session, _ := store.Get(r, "dupa")
+	/*details := LogIn{
+		Username: r.FormValue("username"),
+		Password: r.FormValue("password"),
+	}*/
 
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	rows, err := db.Query(`SELECT username, password FROM users`)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Warn("cos tam chuj")
 	}
-}
+	defer rows.Close()
 
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
-
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		m := validPath.FindStringSubmatch(r.URL.Path)
-		if m == nil {
-			http.NotFound(w, r)
-			return
+	var users []User
+	for rows.Next() {
+		var u User
+		err := rows.Scan(&u.username, &u.password)
+		if err != nil {
+			log.Warn("cos tam dupa")
 		}
-		fn(w, r, m[2])
+		users = append(users, u)
 	}
+	err = rows.Err()
+	println(users)
+	session.Values["authenticated"] = true
+	session.Save(r, w)
+
+	tmpl.Execute(w, struct{ Success bool }{true})
+}
+
+// Logout dupa
+func Logout(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "dupa")
+
+	session.Values["authenticated"] = false
+	session.Save(r, w)
 }
 
 func main() {
-	http.HandleFunc("/view/", makeHandler(viewHandler))
-	http.HandleFunc("/edit/", makeHandler(editHandler))
-	http.HandleFunc("/save/", makeHandler(saveHandler))
+	// ale tu burdel
+	log.SetFormatter(&log.TextFormatter{
+		ForceColors: true,
+	})
+	log.SetOutput(os.Stdout)
+	// TODO: add hashing password with golang.org/x/crypto/bcrypt
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	defer db.Close()
+	err := db.Ping()
+	if err != nil {
+		log.Error("Database ping failure, ", err)
+	}
+	_, err = db.Exec(createTableQuery)
+	if err != nil {
+		log.Error("dupaa fail, ", err)
+	}
+	fs := http.FileServer(http.Dir("site/"))
+	http.Handle("/", http.StripPrefix("/", fs))
+	http.HandleFunc("/secret", Account)
+	http.HandleFunc("/login", Login)
+	http.HandleFunc("/logout", Logout)
+
+	http.ListenAndServe(":8080", nil)
 }
-*/
